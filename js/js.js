67e99d334529e958c4b5703b62bced6adfd3f255
calem48 <messaoud.messaoud92@gmail.com>
2022-08-25 16:21:28 +0100

    edit some buggs

--- js/js.js
+++ js/js.js
@@ -3,13 +3,26 @@ import { UI, Storge, Task } from './index.js';
 //get elements
 let taskForm = document.querySelector('.task-form')
 let list = document.querySelector('.list ul')
+let totale = document.querySelector('.total-item')
+let all = document.querySelector('.all')
+let active = document.querySelector('.active')
+let comp = document.querySelector('.completed')
+let clear = document.querySelector('.clear-item')
 
 // my events
-addEventListener("DOMContentLoaded", () => ui.showTasks(storge.getItem()))
+addEventListener("DOMContentLoaded", () => {
+    totale.innerHTML = storge.getItem().length + " items"
+    ui.showTasks(storge.getItem())
+})
 taskForm.addEventListener("submit", addTask)
 list.addEventListener("click", deleteTask)
 list.addEventListener('click', editTask)
 list.addEventListener('click', complete)
+all.addEventListener('click', allTasks)
+active.addEventListener('click', activeTask)
+comp.addEventListener('click', tasksComplete)
+clear.addEventListener('click', clearAllTasks)
+
 
 //init my objects
 let storge = new Storge()
@@ -21,18 +34,20 @@ let ui = new UI(taskForm, list)
 function addTask(e) {
     e.preventDefault();
 
-    const input = this.querySelector('input[type="text"]').value;
-    if (input === '') {
+    const input = this.querySelector('input[type="text"]');
+    if (input.value === '') {
         return ui.showNotification("please put a value");
     }
 
-
-
     let task = new Task(input)
+
     storge.setItem(task)
     let data = storge.getItem()
     ui.showTasks(data)
+    totale.innerHTML = data.length + " items"
+    input.value = ''
     ui.showNotification("added successfully");
+
 }
 
 
@@ -43,6 +58,7 @@ function deleteTask(e) {
             let data = storge.getItem().filter(item => item.id !== id)
             storge.update(data)
             ui.showTasks(data)
+            totale.innerHTML = data.length
             ui.showNotification("remove successfully")
         }
     }
@@ -90,14 +106,41 @@ function complete(e) {
         ui.showTasks(data)
 
     }
+}
 
-    // let input = e.target.parentElement.previousElementSibling.querySelector('input[type="radio"]')
-    // console.log(input);
+function allTasks() {
 
+
+    let data = storge.getItem()
+    storge.update(data)
+
+    ui.showTasks(data)
+}
+
+function activeTask() {
+    let data = storge.getItem().filter(item => {
+        return !item.complete
+    })
+    // storge.update(data)
+    ui.showTasks(data)
+}
+
+function tasksComplete() {
+    let data = storge.getItem().filter(item => {
+        return item.complete
+    })
+    // storge.update(data)
+
+    ui.showTasks(data)
 }
 
 
-
+function clearAllTasks() {
+    storge.clearAllData()
+    let data = storge.getItem()
+    totale.innerHTML = data.length
+    ui.showTasks(data)
+}
 
 
 
